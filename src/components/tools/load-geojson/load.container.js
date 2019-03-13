38c8901e3fcb55591c0c6ed5545529b9d5a06dde
DanielJDufour <daniel.j.dufour@gmail.com>
2019-03-13 21:27:34 +0000

    some load geojson files

--- src/components/tools/load-geojson/load.container.js
+++ src/components/tools/load-geojson/load.container.js
@@ -1,5 +1,5 @@
 import LoadComponent from './load.component';
-import { addVector } from '../../../actions/raster-actions';
+import { addGeometry } from '../../../actions/geometry-actions';
 import { connect } from 'react-redux';
 import { compose, withState, withHandlers } from 'recompose';
 import { showAlert } from '../../../actions/alert-actions';
@@ -14,7 +14,7 @@ const urlIsValid = url => {
 const mapDispatchToProps = dispatch => {
   return {
     showAlert: message => dispatch(showAlert(message)),
-    addVector: input => dispatch(addVector(input))
+    addGeometry: input => dispatch(addGeometry(input))
   }
 }
 
@@ -29,24 +29,24 @@ export const loadState = compose(
     updateFileInput: ({ setFileInput }) => event => {
       return setFileInput(event.target.files[0]);
     },
-    loadVector: ({ urlInput, fileInput, addVector, showAlert }) => () => {
+    loadGeometry: ({ urlInput, fileInput, addGeometry, showAlert }) => () => {
       return new Promise((resolve, reject) => {
         try {
           if (urlInput !== '' ) {
             if (urlIsValid(urlInput)) {
-              addVector(urlInput);
+              addGeometry(urlInput);
               resolve(true);
             } else {
               showAlert('Please make sure you are using a valid url. It must start with either http or https.');
             }
           } else if (fileInput !== '') {
-            addVector(fileInput);
+            addGeometry(fileInput);
             resolve(true);
           } else {
-            showAlert('Please add either a url or a geotiff file');
+            showAlert('Please add either a url or a geometry file');
           }
         } catch (e) {
-          showAlert('The raster you tried to load is not a valid geotiff. Please try again with a different file.');
+          showAlert('The vector you tried to load is not a valid geometry. Please try again with a different file.');
           reject();
         }
       });
